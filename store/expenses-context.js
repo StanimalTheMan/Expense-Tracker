b96cb3b765ad05b72d576e944eba6099cb677172
StanimalTheMan <sjc591@nyu.edu>
2023-04-26 19:40:08 -0400

    Use context data and update status bar text to white color, add default text when there's no data

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -11,7 +11,7 @@ const DUMMY_EXPENSES = [
     id: "e2",
     description: "A pair of trousers",
     amount: 89.29,
-    date: new Date("2022-01-05"),
+    date: new Date("2023-04-25"),
   },
   {
     id: "e3",
@@ -100,7 +100,18 @@ function ExpensesContextProvider({ children }) {
     dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
   }
 
-  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+  const value = {
+    expenses: expensesState,
+    addExpense: addExpense,
+    deleteExpense: deleteExpense,
+    updateExpense: updateExpense,
+  };
+
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
 }
 
 export default ExpensesContextProvider;
